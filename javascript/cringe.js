97fbd01863cf6edaa84bd6789adfc4b54d954454
sophiafiama <fiama.sophia.sf@gmail.com>
2021-09-03 14:14:53 -0300

    Atualizando arquivo js

--- javascript/cringe.js
+++ javascript/cringe.js
@@ -1,22 +1,23 @@
 
-    // var questao1 = document.quiz.questao1.value;
-    // var questao2 = document.quiz.questao2.value;
-    // var questao3 = document.quiz.questao3.value;
-    // var pontos = parseInt(questao1)  + parseInt(questao2) + parseInt(questao3);
+const form = document.querySelector("#form")
+const nomeInput = document.querySelector("#nome")
+const erroSpan = document.querySelector("#erro")
 
-    const form = document.querySelector("#form")
-    const nomeInput = document.querySelector("#nome")
-    const erroSpan = document.querySelector("#erro")
 
-    form.addEventListener('submit', event=>{
-        event.preventDefault()
 
-        const guardaNome = nomeInput.value.trim()
+form.addEventListener('submit', event => {
+    event.preventDefault()
 
-        if(!guardaNome){
-            erroSpan.innerHTML= '<span>Ops, esse campo é obrigatório!</span>'
-        }
+    const guardaNome = nomeInput.value.trim()
 
-    })
+    if (!guardaNome) {
+        erroSpan.innerHTML = '<span>Ops, esse campo é obrigatório!</span>'
+    }else{
+        console.log("testanto input "+ guardaNome)
+    }
+
+})
+
+
 
 
