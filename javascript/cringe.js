faff781bc3580238ad888ff103c5190ad1a9a6f4
lipollis <aline.pollis@hotmail.com>
2021-09-06 17:29:35 -0300

    nome validado e radio validado

--- javascript/cringe.js
+++ javascript/cringe.js
@@ -16,24 +16,54 @@ form.addEventListener('submit', event => {
         console.log("testanto input "+ guardaNome)
     }
 
-})
-*/
-// ===============================================================================================
+})*/
+// =================================================================================
+//                          VALIDAR NOME
+// =================================================================================
+function validar_nome() {
+    var nome = document.getElementById("nome").value;
+    // RegEX que acceita espaço entre palavras
+    var padrao = /[^a-zà-ú]/gi;
+    var valida_nome = nome.match(padrao);
+    if (valida_nome || !nome) {
+        console.log("Nome possui caracteres inválidos ou é vazio.");
+        document.form.nome.focus();
+        return false;
+    } else {
+        console.log("Nome Ok!");
+        return true;
+    }
+}
+
+
+
+
+// =================================================================================
 //                          VALIDAR RADIO
 // =================================================================================
+var resposta = {};
+var q1 = document.getElementsByName('questao1'); 
+var q2 = document.getElementsByName('questao2');
+var q3 = document.getElementsByName('questao3');
+var q4 = document.getElementsByName('questao4');
+var q5 = document.getElementsByName('questao5');
+var q6 = document.getElementsByName('questao6');
+var q7 = document.getElementsByName('questao7');
 
-function validar_radio(){
+function validar_radio1(){
+
     var radio = document. getElementsByName("questao1");
     for (i=0; i<radio.length;i++){
         if(radio[i].checked){
+
             console.log("radio q1 "+radio[i].value)
         } else{
             console.log("q1 vazio")
         }
     }
-// }
+}
 
-// function validar_radio(){
+function validar_radio2(){
     var radio = document. getElementsByName("questao2");
     for (i=0; i<radio.length;i++){
         if(radio[i].checked){
@@ -42,10 +72,10 @@ function validar_radio(){
             console.log("q2 vazio")
         }
     }
-// }
+}
 
 
-// function validar_radio(){
+function validar_radio3(){
     var radio = document. getElementsByName("questao3");
     for (i=0; i<radio.length;i++){
         if(radio[i].checked){
@@ -54,10 +84,10 @@ function validar_radio(){
             console.log("q3 vazio")
         }
     }
-// }
+}
 
 
-// function validar_radio(){
+function validar_radio4(){
     var radio = document. getElementsByName("questao4");
     for (i=0; i<radio.length;i++){
         if(radio[i].checked){
@@ -66,10 +96,10 @@ function validar_radio(){
             console.log("q4 vazio")
         }
     }
-// }
+}
 
 
-// function validar_radio(){
+function validar_radio5(){
     var radio = document. getElementsByName("questao5");
     for (i=0; i<radio.length;i++){
         if(radio[i].checked){
@@ -78,9 +108,9 @@ function validar_radio(){
             console.log("vazio q5")
         }
     }
-// }
+}
 
-// function validar_radio(){
+function validar_radio6(){
     var radio = document. getElementsByName("questao6");
     for (i=0; i<radio.length;i++){
         if(radio[i].checked){
@@ -89,10 +119,10 @@ function validar_radio(){
             console.log("vazio q6")
         }
     }
-// }
+}
 
 
-// function validar_radio(){
+function validar_radio7(){
     var radio = document. getElementsByName("questao7");
     var contagem7 = 0
     for (i=0; i<radio.length;i++){
@@ -103,47 +133,57 @@ function validar_radio(){
             console.log("vazio q7")
         }
     }
+
+    
+
+
 }
+
+
+
+function resposta(q_num, event){
+    if(event.target.type === 'radio'){
+        console.log(event.target.value);
+        resposta['questão '+q_num]=parseInt(event.target.value);
+        console.log(resposta)
+    }
+}
+
 /*
-    function validar_radio() {
-    var alternativa_01 = document.querySelector("input[id^='alternativa_01']:checked");
-    var alternativa_02 = document.querySelector("input[id^='alternativa_02']:checked");
-    var alternativa_03 = document.querySelector("input[id^='alternativa_03']:checked");
-    var alternativa_04 = document.querySelector("input[id^='alternativa_04']:checked");
 
-    // var alternativa_01 = document.getElementById("input[id^='alternativa_01']:checked");
-    // var alternativa_02 = document.getElementById("input[id^='alternativa_02']:checked");
-    // var alternativa_03 = document.getElementById("input[id^='alternativa_03']:checked");
-    // var alternativa_04 = document.getElementById("input[id^='alternativa_04']:checked");
+q2.addEventListener('click',function(event){
+    resposta(2,event)
+})
 
+q3.addEventListener('click',function(event){
+    resposta(3,event)
+})
 
-    if (alternativa_01.checked == true) {
-        // alert("1")
-        console.log("Toma café");
-        return true
-    }else if (alternativa_02.checked == true) {
-        // alert("2")
-        console.log("assisti jornal");
-        return true
-    }
-   else if (alternativa_03.checked == true) {
-    // alert("3")
-        console.log("olha cel");
-        return true
-    }
-    else if (alternativa_04.checked == true) {
-        // alert("4")
-        console.log("vai pra aula");
-        return true
-    }
-    // else if (     (alternativa_01.checked == false) 
-    //             && (alternativa_02.checked == false)
-    //             && (alternativa_03.checked == false)
-    //             && (alternativa_04.checked == false)
-    //             ){
-    //     console.log("Campo não selecionado.");
-    //     return false
-        
-    // }
-}
-*/
+q4.addEventListener('click',function(event){
+    resposta(4,event)
+})
+
+q5.addEventListener('click',function(event){
+    resposta(5,event)
+})
+
+q6.addEventListener('click',function(event){
+    resposta(6,event)
+})
+
+q7.addEventListener('click',function(event){
+    resposta(7,event)
+})
+
+function total(){
+    var questao_total =
+    resposta.q1+
+    resposta.q2+
+    resposta.q3+
+    resposta.q4+
+    resposta.q5+
+    resposta.q6+
+    resposta.q7;
+
+    return questao_total;
+}*/
